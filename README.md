823ea25ef46feecc858e4d0b8589597a1458bf13
Josimar Minete <josimaminete@gmail.com>
2021-09-20 23:20:52 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# mudi_spring_mvc
+ Projeto Spring Mvc com app usando tymeleaf e Bootstrap
---
+++ src/test/java/br/com/alura/mvc/mudi_spring_mvc/MudiSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.mvc.mudi_spring_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudiSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
