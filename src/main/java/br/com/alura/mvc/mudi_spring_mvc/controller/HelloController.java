27aac2ef2cbf156966c318c14bb3366fa2c73492
Josimar Minete <josimaminete@gmail.com>
2021-09-20 23:30:55 -0300

    Fazendo mais alguns testes.

--- src/main/java/br/com/alura/mvc/mudi_spring_mvc/controller/HelloController.java
+++ src/main/java/br/com/alura/mvc/mudi_spring_mvc/controller/HelloController.java
@@ -1,13 +1,16 @@
 package br.com.alura.mvc.mudi_spring_mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 
 	@GetMapping("/hello")
-	public String hello() {
+	public String hello(Model model) {
+		model.addAttribute("nome", "Mundo");
+		model.addAttribute("teste", "Teste");
 		return "hello";
 	}
 }
