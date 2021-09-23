e0ad8dba3b7d0cfab8688500f9b425a42d804a4d
Josimar Minete <josimaminete@gmail.com>
2021-09-22 23:05:01 -0300

    Adicionado um repositório para gerenciar melhor as consultas.

--- src/main/java/br/com/alura/mvc/mudi_spring_mvc/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi_spring_mvc/controller/HomeController.java
@@ -3,32 +3,25 @@ package br.com.alura.mvc.mudi_spring_mvc.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceContexts;
-import javax.persistence.Query;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.alura.mvc.mudi_spring_mvc.model.Pedido;
+import br.com.alura.mvc.mudi_spring_mvc.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
 
-	@PersistenceContext
-	private EntityManager entityManager;
+	// @Autowired cria uma instancia ao iniciar a classe
+	@Autowired
+	private PedidoRepository pedidoRepository;
 
 	@GetMapping("/home")
 	public String home(Model model) {
-
-		// Pedido.class representa o tipo que a consulta irá retornar
-		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
-		List<Pedido> pedidos = query.getResultList();
-
+		List<Pedido> pedidos = pedidoRepository.recuperarTodosPedidos();
 		model.addAttribute("pedidos", pedidos);
-
 		return "home";
 	}
 }
