9b9425477b313be4965959e453a904a32e22aee6
Josimar Minete <josimaminete@gmail.com>
2021-09-29 22:06:44 -0300

    Adicionado login com dados do banco de dados.

--- src/main/java/br/com/alura/mvc/mudi_spring_mvc/WebSecurityConfig.java
+++ src/main/java/br/com/alura/mvc/mudi_spring_mvc/WebSecurityConfig.java
@@ -1,19 +1,28 @@
 package br.com.alura.mvc.mudi_spring_mvc;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource dataSource;
+	
+	// configuração de autorização
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
@@ -25,17 +34,37 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.and()
 		.formLogin(form -> form
             .loginPage("/login")
+            .defaultSuccessUrl("/home", true)
             .permitAll()
         )
 		.logout(logout -> logout.logoutUrl("/logout"));
 	}
 
-	@Bean
+	//indicar que ai trabalhar com jdbc autentication
 	@Override
-	public UserDetailsService userDetailsService() {
-		UserDetails user = User.withDefaultPasswordEncoder().username("oi").password("oi").roles("ADM").build();
-
-		return new InMemoryUserDetailsManager(user);
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		
+		//criar usuário inicial
+		//UserDetails user = User.builder().username("joao").password(encoder.encode("joao")).roles("ADM").build();
+		System.out.println("Teste");
+		System.out.println(encoder.encode("josimar"));
+		//System.out.println(user.getPassword());
+		System.out.println(encoder);
+		
+		auth
+		.jdbcAuthentication()
+		.dataSource(dataSource)
+		.passwordEncoder(encoder);
+		//.withUser(user);
 	}
+	
+//	@Bean
+//	@Override
+//	public UserDetailsService userDetailsService() {
+//		UserDetails user = User.withDefaultPasswordEncoder().username("oi").password("oi").roles("ADM").build();
+//
+//		return new InMemoryUserDetailsManager(user);
+//	}
 
 }
