b05140bbf12f9c377273dab50c76c770db0bda0a
Josimar Minete <josimaminete@gmail.com>
2021-10-10 20:18:28 -0300

    Adicionando cache na aplicação.

--- src/main/java/br/com/alura/mvc/mudi_spring_mvc/MudiSpringMvcApplication.java
+++ src/main/java/br/com/alura/mvc/mudi_spring_mvc/MudiSpringMvcApplication.java
@@ -2,7 +2,9 @@ package br.com.alura.mvc.mudi_spring_mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching // para inicializar o cash junto com a aplicação
 @SpringBootApplication
 public class MudiSpringMvcApplication {
 
--- src/main/java/br/com/alura/mvc/mudi_spring_mvc/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/mvc/mudi_spring_mvc/repository/PedidoRepository.java
@@ -2,8 +2,8 @@ package br.com.alura.mvc.mudi_spring_mvc.repository;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,6 +14,8 @@ import br.com.alura.mvc.mudi_spring_mvc.model.StatusPedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+	
+	@Cacheable("books")
 	List<Pedido> findByStatus(StatusPedido aguardando, Pageable sort);
 
 	@Query("select p from Pedido p join p.user u where u.username = :username")
